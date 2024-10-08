63eb63160c34df5d93cd44fc77860e05821ab010
ryanhowe11 <115746867+ryanhowe11@users.noreply.github.com>
2024-10-08 15:30:52 -0600

    Updates

--- Project 3.jl
+++ Project 3.jl
@@ -4,6 +4,7 @@ using QuadGK
 using Interpolations
 using Trapz
 using SNOW
+using Ipopt
 
 # Define inputs of function
 span = 4 #one wing or the whole span       
@@ -12,17 +13,44 @@ weight = 1.7
 Vinf = 1.0
 
 #Creating the optimization problem
-function optimize_wing()
+function wing_optimizer()
     model = Model(Ipopt.Optimizer)
 
     #Define variables
-    @variable(model, x[1:7], start = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
-    @variable(model, c[1:7], start = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4])
+    @variable(model, x[1:7])
+    @variable(model, c[1:7])
+    
+    #Create initial arrays for x and c
+    initial_x_values = zeros(7)
+    initial_c_values = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+
+    for i in 1:6
+        set_start_value(x[i], initial_x_values[i])
+        set_start_value(c[i], initial_c_values[i])
+    end
+
+    #=
+    @variable(model, x[1] == 0.0)
+    @variable(model, x[2] == 0.0)
+    @variable(model, x[3] == 0.0)
+    @variable(model, x[4] == 0.0)
+    @variable(model, x[5] == 0.0)
+    @variable(model, x[6] == 0.0)
+    @variable(model, x[7] == 0.0)
+
+    @variable(model, c[1] == 4.0)
+    @variable(model, c[2] == 4.0)
+    @variable(model, c[3] == 4.0)
+    @variable(model, c[4] == 4.0)
+    @variable(model, c[5] == 4.0)
+    @variable(model, c[6] == 4.0)
+    @variable(model, c[7] == 4.0)
+=#
 
     #Define constraints for the Optimizer
     for i in 1:6
-        @constraint(model, x[i+1] > x[i])
-        @constraint(model, c[i] > c[i+1])
+        @constraint(model, x[i+1] >= x[i])
+        @constraint(model, c[i] >= c[i+1])
     end
 
 # geometry (right half of the wing)
@@ -39,6 +67,14 @@ ns = 7
 nc = 6
 spacing_s = Uniform()
 spacing_c = Uniform()
+
+Sref= 0.0
+
+# Reference Area Calculation
+for i in 1:6
+    global Sref
+    Sref += ((chord[i] + chord[i+1]) / 2) * (yle[i+1] - yle[i])
+end
 
 # reference parameters
 rref = [0.50, 0.0, 0.0]
@@ -67,42 +103,49 @@ system = steady_analysis(surfaces, ref, fs; symmetric=symmetric)
 # retrieve near-field forces
 CF, CM = body_forces(system; frame=Wind())
 
-# perform far-field analysis
-CDiff = far_field_drag(system)
-
 CD, CY, CL = CF
 Cl, Cm, Cn = CM
 
-dCF, dCM = stability_derivatives(system)
-
-CDa, CYa, CLa = dCF.alpha
-Cla, Cma, Cna = dCM.alpha
-CDb, CYb, CLb = dCF.beta
-Clb, Cmb, Cnb = dCM.beta
-CDp, CYp, CLp = dCF.p
-Clp, Cmp, Cnp = dCM.p
-CDq, CYq, CLq = dCF.q
-Clq, Cmq, Cnq = dCM.q
-CDr, CYr, CLr = dCF.r
-Clr, Cmr, Cnr = dCM.r
-
 properties = get_surface_properties(system)
 
-write_vtk("symmetric-planar-wing", surfaces, properties; symmetric)
+write_vtk("optimized-symmetric-planar-wing", surfaces, properties; symmetric)
 
-grids=[grid]
+L=.5*rho*Vinf^2*Sref*CL
+D=.5*rho*Vinf^2*Sref*CD
 
-r, c = lifting_line_geometry(grids, 0.25)
+#Set forth objective to minimize drag
+@objective(model, Min, D)
 
-cf, cm = lifting_line_coefficients(system, r, c; frame=Wind())
+#Lift constraint
+@constraint(model, L >= weight)
 
-A = 0.0  # Initialize the area
+# Solve the optimization problem
+optimize!(model)
 
-# Ensure A is treated as a local variable within the loop
-for i = 1:6
-    global A
-    A += ((chord[i] + chord[i+1]) / 2) * (yle[i+1] - yle[i])
+# Extract optimized values
+x_opt = value.(x)
+c_opt = value.(c)
+
+#update the geometry with optimized values
+xle_opt = x_opt
+chord_opt = c_opt
+
+# reconstruct surface
+grid_opt, surface_opt = wing_to_surface_panels(xle_opt, yle, zle, chord_opt, theta, phi, ns, nc;
+    spacing_s=spacing_s, spacing_c=spacing_c)
+
+# create vector containing all surfaces
+surfaces_opt = [surface_opt]
+
+system = steady_analysis(surfaces_opt, ref, fs; symmetric=symmetric)
+
+properties_opt = get_surface_properties(system)
+
+write_vtk("symmetric-planar-wing", surfaces_opt, properties_opt; symmetric=true)
+
+return x_opt, c_opt
 end
 
-L=.5*rho*Vinf^2*A*CL
-D=.5*rho*Vinf^2*A*CD
+x_opt, c_opt = wing_optimizer()
+println("Optimized leading edge values: ", x_opt)
+println("Optimized chord values: ", c_opt)
