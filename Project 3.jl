c94a715b937615e108b2b5de5efa3adb0cf103f9
ryanhowe11 <115746867+ryanhowe11@users.noreply.github.com>
2024-10-08 17:52:12 -0600

    Updates

--- Project 3.jl
+++ Project 3.jl
@@ -1,67 +1,32 @@
-using Plots
 using VortexLattice
-using QuadGK
-using Interpolations
-using Trapz
 using SNOW
 using Ipopt
+using FiniteDiff
+using ForwardDiff
 
-checkallocations = false
-snopttest = false
-
-@testset "derivatives" begin
+#Creating the optimization problem
+function wing_optimizer(g, c)
 
 # Define inputs of function
-span = 4 #one wing or the whole span       
+span = 4.0 #one wing or the whole span       
 rho = 1.225
 weight = 1.7
 Vinf = 1.0
 
-#Creating the optimization problem
-function wing_optimizer!(x, c)
-    model = Model(Ipopt.Optimizer)
-
-    #Define variables
-    @variable(model, x[1:7])
-    @variable(model, c[1:7])
-
-    for i in 1:6
-        set_start_value(x[i], 0)
-        set_start_value(c[i], 1)
-    end
-
-    #=
-    @variable(model, x[1] == 0.0)
-    @variable(model, x[2] == 0.0)
-    @variable(model, x[3] == 0.0)
-    @variable(model, x[4] == 0.0)
-    @variable(model, x[5] == 0.0)
-    @variable(model, x[6] == 0.0)
-    @variable(model, x[7] == 0.0)
-
-    @variable(model, c[1] == 4.0)
-    @variable(model, c[2] == 4.0)
-    @variable(model, c[3] == 4.0)
-    @variable(model, c[4] == 4.0)
-    @variable(model, c[5] == 4.0)
-    @variable(model, c[6] == 4.0)
-    @variable(model, c[7] == 4.0)
-=#
-
-    #Define constraints for the Optimizer
-    for i in 1:6
-        @constraint(model, x[i+1] >= x[i])
-        @constraint(model, c[i] >= c[i+1])
-    end
-
 # geometry (right half of the wing)
-xle = x
 yle = (0, 2/3, 4/3, 2, 8/3, 10/3, 4)
-zle = (0, 0, 0, 0, 0, 0 ,0)
-chord = c
+zle = (0, 0, 0, 0, 0, 0, 0)
 theta = fill(0, 7)
 phi = zeros(7)
+chords=c
 fc = fill((xc) -> 0, 7)                     # camberline function for each section
+
+xle=zeros(7)
+
+for i in 1:6
+xle[i+1]=c[i]/4-c[i+1]/4
+end
+
 
 # discretization parameters
 ns = 7
@@ -74,12 +39,14 @@ Sref= 0.0
 # Reference Area Calculation
 for i in 1:6
     global Sref
-    Sref += ((chord[i] + chord[i+1]) / 2) * (yle[i+1] - yle[i])
+    Sref += ((c[i] + c[i+1]) / 2) * (yle[i+1] - yle[i])
 end
+
+x=xle[1]
 
 # reference parameters
 rref = [0.50, 0.0, 0.0]
-ref = Reference(Sref, x[1], span, rref, Vinf)
+ref = Reference(Sref, x, span, rref, Vinf)
 
 
 # freestream parameters
@@ -89,7 +56,7 @@ Omega = [0.0; 0.0; 0.0]
 fs = Freestream(Vinf, alpha_angle, beta, Omega)
 
 # construct surface
-grid, surface = wing_to_surface_panels(xle, yle, zle, chord, theta, phi, ns, nc;
+grid, surface = wing_to_surface_panels(xle, yle, zle, chords, theta, phi, ns, nc;
     spacing_s=spacing_s, spacing_c=spacing_c)
 
 # create vector containing all surfaces
@@ -111,42 +78,51 @@ properties = get_surface_properties(system)
 
 write_vtk("optimized-symmetric-planar-wing", surfaces, properties; symmetric)
 
-L=.5*rho*Vinf^2*Sref*CL
+
 D=.5*rho*Vinf^2*Sref*CD
 
-#Set forth objective to minimize drag
-@objective(model, Min, D)
+g[1]=.5*rho*Vinf^2*Sref*CL-weight
 
-#Lift constraint
-@constraint(model, L >= weight)
+for i in 1:6
+g[1+i]=x[i]-x[i+1]
+g[8+i]=c[i+1]-c[i]
+end
 
-# Solve the optimization problem
-optimize!(model)
+return D
+end
 
-# Extract optimized values
-x_opt = value.(x)
-c_opt = value.(c)
+c0 = [1.0; 1.0; 1.0; 1.0; 1.0; 1.0; 1.0]  # starting point
+lc = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]  # lower bounds on x
+uc = [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]  # upper bounds on x
+ng = 13  # number of constraints
+lg = -Inf*one(ng)  # lower bounds on g
+ug = zeros(ng)  # upper bounds on g
 
-#update the geometry with optimized values
-xle_opt = x_opt
-chord_opt = c_opt
+# ----- set some options ------
+ip_options = Dict(
+    "max_iter" => 3,
+    "tol" => 1e-6
+)
+solver = IPOPT(ip_options)
+options = Options(;solver)
 
+xopt, fopt, info = minimize(wing_optimizer, c0, ng, lc, uc, lg, ug, options)
+
+#=
 # reconstruct surface
-grid_opt, surface_opt = wing_to_surface_panels(xle_opt, yle, zle, chord_opt, theta, phi, ns, nc;
+grid_opt, surface_opt = wing_to_surface_panels(x_opt, yle, zle, c_opt, theta, phi, ns, nc;
     spacing_s=spacing_s, spacing_c=spacing_c)
 
 # create vector containing all surfaces
 surfaces_opt = [surface_opt]
 
-system = steady_analysis(surfaces_opt, ref, fs; symmetric=symmetric)
+system_opt = steady_analysis(surfaces_opt, ref, fs; symmetric=symmetric)
 
-properties_opt = get_surface_properties(system)
+properties_opt = get_surface_properties(system_opt)
 
 write_vtk("symmetric-planar-wing", surfaces_opt, properties_opt; symmetric=true)
-
-return x_opt, c_opt
-end
 
 x_opt, c_opt = wing_optimizer()
 println("Optimized leading edge values: ", x_opt)
 println("Optimized chord values: ", c_opt)
+=#
