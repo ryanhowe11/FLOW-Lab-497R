a87efbeaf54f25f64a8bf6ab0c426b4917251844
ryanhowe11 <115746867+ryanhowe11@users.noreply.github.com>
2024-11-27 09:16:51 -0700

    updates

--- Final Project/OptimizedRangeTotalAirframeFunction.jl
+++ Final Project/OptimizedRangeTotalAirframeFunction.jl
@@ -139,7 +139,7 @@ function OptimizationSetup(num_sec, xstart)
  # ----- set some options ------
  ip_options = Dict(
      "max_iter" => 1250,
-     "tol" => 1e-2
+     "tol" => 1e-5
  )
  solver = IPOPT(ip_options)
  options = Options(;solver, derivatives=ForwardFD())
